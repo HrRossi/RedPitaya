0c3cb52ac9e2cdc35e0b598421aa9ffeb75f121d
HrRossi <security@techno.ms>
2016-02-27 18:30:25 +0100

    Vivado 2015.4: IPs migrated, errors in build fixed
    the project and scripts have been changed to allow the logic to be opened and
    changed with Vivado 2015.4 (the current supported version on master).
    during the Makefile build, the right project environment will be detected
    automatically depending on the settings64.sh that has been executed before.
    
    notice: due to changes in the Xilinx SDK, the Makefile build will fail for the
    2015.4 environment right after the bitstream and hardware definitions have been
    generated.

--- FPGA/release1/fpga/run_vivado.tcl
+++ FPGA/release1/fpga/run_vivado.tcl
@@ -7,15 +7,22 @@
 #   clean   cleans project files
 #   build   build all output files
 
+set current_vivado_version [version -short]
 
 
 #*******************************************************************************
 # Open project
-open_project ./vivado/red_pitaya.xpr
+
+if { [string first 2013.3 $current_vivado_version] == -1 } {
+   open_project ./vivado2015_4/red_pitaya.xpr
+} else {
+   open_project ./vivado/red_pitaya.xpr
+}
 
 
 #*******************************************************************************
 # Update sources
+
 update_compile_order -fileset sources_1
 
 
@@ -30,10 +37,12 @@ if {[lindex $argv 0] == "clean"} {
    reset_run synth_1
 
    ## clean PS project configuration
-   reset_target all [get_files  ./vivado/red_pitaya.srcs/sources_1/bd/system/system.bd]
+   if { [string first 2013.3 $current_vivado_version] == -1 } {
+      reset_target all [get_files  ./vivado2015_4/red_pitaya.srcs/sources_1/bd/system/system.bd]
+   } else {
+      reset_target all [get_files  ./vivado/red_pitaya.srcs/sources_1/bd/system/system.bd]
+   }
 }
-
-
 
 
 ##*******************************************************************************
@@ -42,9 +51,16 @@ if {[lindex $argv 0] == "clean"} {
 if {[lindex $argv 0] == "build"} {
 
    ## export PS configuration
-   generate_target all [get_files  ./vivado/red_pitaya.srcs/sources_1/bd/system/system.bd]
-   open_bd_design ./vivado/red_pitaya.srcs/sources_1/bd/system/system.bd
-   export_hardware [get_files ./vivado/red_pitaya.srcs/sources_1/bd/system/system.bd]
+   if { [string first 2013.3 $current_vivado_version] == -1 } {
+      generate_target all [get_files  ./vivado2015_4/red_pitaya.srcs/sources_1/bd/system/system.bd]
+      open_bd_design ./vivado2015_4/red_pitaya.srcs/sources_1/bd/system/system.bd
+      file mkdir ./vivado/red_pitaya.sdk/SDK/SDK_Export/hw/
+      write_hwdef -file ./vivado/red_pitaya.sdk/SDK/SDK_Export/hw/system.hwdef
+   } else {
+      generate_target all [get_files  ./vivado/red_pitaya.srcs/sources_1/bd/system/system.bd]
+      open_bd_design ./vivado/red_pitaya.srcs/sources_1/bd/system/system.bd
+      export_hardware [get_files ./vivado/red_pitaya.srcs/sources_1/bd/system/system.bd]
+   }
    close_bd_design system
 
    ## do synthesis
@@ -61,12 +77,10 @@ if {[lindex $argv 0] == "build"} {
 }
 
 
-
-
 #*******************************************************************************
 # Close opened project
-close_project
 
+close_project
 
 
 exit
